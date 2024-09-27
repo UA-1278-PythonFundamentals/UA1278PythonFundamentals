93ef84585095cc7c35713878e6a37d8e46ca0f95
ANDRII MAKOVETSKYI <Shmak@ukr.net>
2024-09-27 13:10:27 +0300

    hw/hw03/mak - HW.py (#12)

---
+++ hw/hw03/amakovetskiy-HW3.py
@@ -0,0 +1,25 @@
+t = True
+print(type(t), t) 
+t = False
+print(type(t), t)
+# t = 1145
+t = 1145
+print(type(t), t)
+t = 55.5 
+print(type(t), t)
+t = [55.5, 55]
+print(type(t), t)
+t = (55.5, 55)
+print(type(t), t)
+t = "(55.5, 55)"
+print(type(t), t)
+t = {2,5,6,4,7,5,6,2,5,7,9}
+print(type(t), t)   
+t = set("dfvbcbbvmbncvbcgjfkvhnglkg")
+print(type(t), t) 
+
+a = "45"
+print(id(a))
+a = a + "8"
+print(id(a), a)
+
