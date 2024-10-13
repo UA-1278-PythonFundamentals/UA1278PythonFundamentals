8ceeff9543e1ce95867e8183198ff9c978c4f594
Halyna Fedkiv <haliafedkiv@gmail.com>
2024-10-13 21:35:30 +0300

    hw_08

--- hw/hw08/HalynaFedkiv/Task_2.py
+++ hw/hw08/HalynaFedkiv/Task_2.py
@@ -2,13 +2,8 @@ import re
 
 
 def password_validation(password):
-    if re.search('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$#@])[A-Za-z0-9$#@]{6,16}$', password):
-        return True
-    else:
-        return False
+    return re.search('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$#@])[A-Za-z0-9$#@]{6,16}$', password)
 
 
-print(password_validation('jkhvj9fkd9'))
-print(password_validation('kRjigf@kd80'))
-print(password_validation('1Rjf@!!'))
-print(password_validation('1Rj@'))
+user_input = input('Enter a password: ')
+print(f'Your password is {"valid" if password_validation(user_input) else "invalid"}.')
