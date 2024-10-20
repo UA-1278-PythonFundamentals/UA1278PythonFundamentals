4bbfcf70c5213782d880c6cd65e2614b3012f3f2
DenRohachov <denis.comfy@gmail.com>
2024-10-20 21:14:20 +0300

    DenRohachov finished HW04

--- hw/hw04/DenRohachov/HW-04.py
+++ hw/hw04/DenRohachov/HW-04.py
@@ -1,1 +1,8 @@
 ### HW 4
+
+temperature = float(input("Вкажить температуру в цельсія: "))
+convert = (temperature * 9/5) + 32
+if temperature >= -273.15:
+    print(float(temperature), "C = ", convert, " F")
+else:
+    print("Error: Temperature below absolute zero (-273.15 C)\nInput is:",temperature, "C")
