b1e10ebc26cfb8841584a0e9a335acf9668fad82
Andrii Makovetskyi <shmak@ukr.net>
2024-10-29 11:23:54 +0200

    hw/hw06/amakovetskiy/hw_6.py

--- hw/hw06/amakovetskiy/hw_6.py
+++ hw/hw06/amakovetskiy/hw_6.py
@@ -1,3 +1,34 @@
-l=1
+# Task1. In the range from 1 to 10 determine
+# • even numbers that are divisible by 2,
+# • odd numbers, which are divisible by 3,
+# • numbers that are not divisible by 2 and 3.
 
-k=2
+print(print ("\n\n\n\n\n##############################(Task1)#################################\n\n\n\n"))
+
+
+even_numbers  = []
+odd_numbers   = []
+not_divisible = []
+for i in range(1, 11):
+    if i % 2 == 0:
+       even_numbers.append(i)
+    if i % 3  == 0: 
+        odd_numbers.append(i)
+    if i % 2 != 0 and i % 3 != 0:
+        not_divisible.append(i)
+
+print(f'Even numbers is: {even_numbers}')
+print(f'Odd numbers is: {odd_numbers}')
+print(f'Not divisible is: {not_divisible}')
+
+
+# COMPREHENSIONS
+
+even_num2 = [i for i in range(1, 11) if i % 2 == 0]
+print(even_num2)
+
+odd_num2 = [i for i in range(1, 11) if i % 3 == 0]
+print(odd_num2)
+
+not_divisible2 = [i for i in range(1, 11) if i % 2 != 0 and i % 3 != 0]
+print(not_divisible2)
