13eb4f121e5dacc6b633c0cb1b08bb9f4035e44d
Andrii Makovetskyi <shmak@ukr.net>
2024-10-30 18:36:16 +0200

    hw/hw07/amakovetskiy/HW_7.py

--- hw/hw07/amakovetskiy/HW_7.py
+++ hw/hw07/amakovetskiy/HW_7.py
@@ -67,3 +67,25 @@ elif string == 'c':
     print(area_of_circle(radius=int(input(f'Enter the radius of circle: ==>  '))))
 else:
     print('Error: letter entered incorrectly')
+
+
+
+    print ("\n\n\n\n\n##############################(Task3)#################################\n\n\n\n")
+
+
+    # Task3. Write a function that calculates the number of characters
+# included in a given string
+# • input: "hello"
+# • output: {"h":1, "e":1,"l":2,"o":1}
+
+def count_letters(word='hello'):
+    letter_count = {}
+    for i in list(word):
+        if i in letter_count:
+            letter_count[i] += 1
+        else:
+            letter_count[i] = 1
+    return letter_count
+
+
+print(count_letters())
