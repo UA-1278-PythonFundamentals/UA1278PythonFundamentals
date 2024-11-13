c53b8b294d91fb595a080f3ff1e46cef1fc66caa
Liubomyr Halamaha <galamagal86@gmail.com>
2024-11-13 12:35:32 +0200

    update path VladyslavPlakhotniuk

--- hw/__init__.py
+++ hw/__init__.py
@@ -1,4 +1,14 @@
 import os
-print(os.getcwd()+"\\hw")
-for path in os.walk(os.getcwd()+"\\hw"):
-    print(path)
+
+# print(os.getcwd() + "\\hw")
+d = {}
+for path in os.walk(os.getcwd() + "\\hw"):
+    s_path = path[0].split("\\")
+    if len(s_path) == 7:
+        # print(s_path)
+        if s_path[6] in d:
+            d[s_path[6]].append(s_path[5])
+        else:
+            d[s_path[6]] = [s_path[5]]
+from pprint import pprint
+pprint(d)
---
+++ temp.py
@@ -0,0 +1,84 @@
+# a = 1
+# print(type(a), a)
+# a = "1"
+# print(type(a), a)
+# a = [1, 2, 3]
+# print(type(a), a)
+# a = 2.6
+# print(type(a), a)
+
+# a = 999**1000
+# print(a)
+# print(len(str(a)))
+# print(str(a).count("9"))
+# import this
+
+# a = 1+1+1+1+1+1+1+1
+# print(f"{a=}")
+
+# a = 1+1+1+1 \
+#         +1+1+1+1
+# print(f"{a=}")
+# print("tete", 10, "<>")
+# print("test")
+# # tete 10 <>
+
+# print("tete", 10, "<>", sep="----")
+# print("tete", 10, "<>", sep="+++", end=" ==>> ")
+# print("tete", 10, "<>", sep="----")
+# print("te")
+
+# a = 10
+
+# a = [10,20,30]
+# for  i in a:
+#     print(i)
+# for i in range(len(a)):
+#     print(a[i])
+
+# import keyword
+# kw = keyword.kwlist
+# print(len(kw), kw)
+
+# printThisToScreen = "100"
+# print(printThisToScreen)
+# print_this_to_screen = "199"
+# print(print_this_to_screen)
+
+# a1qa1 = "sss"
+# print(a1qa1)
+# # 1asdsad = 99 #SyntaxError: invalid decimal literal
+
+# a = 1
+# print(a)
+
+# aA =1
+# Aa = 2
+# print(aA, Aa)
+# print(aA == Aa)
+# print(aA == 1)
+
+# a = 7
+# b = 3
+
+# print(f"{a=} {b=} {a+b=}")
+# print(f"{a=} {b=} {a-b=}")
+# print(f"{a=} {b=} {a*b=}")
+# print(f"{a=} {b=} {a/b=}")
+# print(f"{a=} {b=} {a**b=}")
+# print(f"{a=} {b=} {a%b=}")
+# print(f"{a=} {b=} {a//b=}")
+
+
+# print("a="+str(a) + " b=" + str(b) )
+# print(".=<>=."*8 )
+
+
+# for i in range(3):
+#         print(i)
+#         print(i)
+
+print("What is your name?")
+name = input("my name is:")
+print("Hello", name)
+  
